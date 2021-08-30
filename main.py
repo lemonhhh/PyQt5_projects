d3d84edbf3afd1137a6a15754ae3a30ff341516e
LemonHhh <7520951+lemonhhh@user.noreply.gitee.com>
2021-08-30 19:15:56 +0800

    0830

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 
-print("hi")
+
+from PyQt5.Qt import *
+import sys
+
+
+
+app = QApplication(sys.argv)
+
+window = QWidget()
+
+window.setWindowTitle("QLabel的学习")
+window.resize(500,500)
+
+label = QLabel(window)
+label.setText("xxx")
+
+window.show()
+sys.exit(app.exec_())
+
+
